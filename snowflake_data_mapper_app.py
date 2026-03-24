141fa9c0feba2e006964c3c7f7b7349bc8a85ce4
Lalit Maan <mysantech.in@gmail.com>
2026-03-24 16:04:12 +0530

    Update logo image source in sidebar

--- snowflake_data_mapper_app.py
+++ snowflake_data_mapper_app.py
@@ -44,7 +44,7 @@ st.markdown("""
 # SIDEBAR / CONTACT DETAILS
 # ---------------------------------------------------------------------------------
 with st.sidebar:
-    st.image("https://github.com/Lalitmaan988/Reconciliation-Tool-By-Lalit-Maan/blob/main/Logo.png", width=60)
+    st.image("https://drive.google.com/file/d/1xr5h5rX0O67kbHIlKOa6AwIcz7k5CUkX/view?usp=sharing", width=60)
     st.markdown("## 👨‍💻 About the Developer")
     st.info("**Built to streamline CRM data management, reduce duplicate entries, and automate complex workflows.**")
     
